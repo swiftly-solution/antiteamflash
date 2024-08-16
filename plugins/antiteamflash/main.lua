1c92e2a2c7c4cb7459f61e8604062a91fa1838ab
blu <73272097+blu133721@users.noreply.github.com>
2024-08-16 14:26:03 +0300

    src(verifications): Add verifications.

--- plugins/antiteamflash/main.lua
+++ plugins/antiteamflash/main.lua
@@ -6,7 +6,9 @@ AddEventHandler("OnPlayerBlind", function(event)
 	local attacker = GetPlayer(attackerid)
 	if not attacker then return EventResult.Continue end
 
+	if not player:CBaseEntity():IsValid() then return end
 	if player:CBaseEntity().TeamNum == attacker:CBaseEntity().TeamNum and (not config:Fetch("antiteamflash.blind_thrower") or playerid ~= attackerid) then
+		if not player:CCSPlayerPawnBase():IsValid() then return end
 		player:CCSPlayerPawnBase().BlindUntilTime = server:GetCurrentTime()
 	end
         
